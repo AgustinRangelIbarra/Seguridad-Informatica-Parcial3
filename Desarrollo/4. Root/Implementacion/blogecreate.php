f6976d024db3d4aa361ea9b5408e3f331d93a54e
Franco Mp <stha-182@hotmail.com>
2019-11-19 21:40:20 -0600

    2019/11/19 21:40 Root Services

--- Desarrollo/4. Root/Implementacion/blogecreate.php
+++ Desarrollo/4. Root/Implementacion/blogecreate.php
@@ -10,18 +10,9 @@
          $request = json_decode(file_get_contents("php://input"), true);
 
     $requestEmail = $request['email'];
-    $requestfirstname= $request ['firstname'];
-    $requestPassword = $request['password'];
-    $requestlastname=$request['lastname'];
-    $requestDaysOfPasswordValidity = $request['DaysOfPasswordValidity'];
-    $requestisTemporalPassword = $request['isTemporalPassword'];
-    $requestActivationKey = $request['ActivationKey'];
-    $requestStatus = $request['Status'];
-    $requestQiz1 = $request['Qiz1'];
-    $requestQiz2 = $request['Qiz2'];
-    $requestQiz3 = $request['Qiz3'];
+    $requestValue= $request ['value'];
 
-    $query = "INSERT INTO users VALUES ('". $requestEmail ."', '".$requestPassword."', '".$requestfirstname."', '".$requestlastname."', '".$requestDaysOfPasswordValidity."', NOW(), '".$requestisTemporalPassword."', '".$requestActivationKey."', '".$requestStatus."','".$requestQiz1."','".$requestQiz2."','".$requestQiz3."')";
+    $query = "INSERT INTO blog_entries (user_email, entry_value, registration_date) VALUES ('".$requestEmail."', '".$requestValue."', now())";
         $stmt = mysqli_prepare($dbc, $query);
         mysqli_stmt_execute($stmt);
         $affected_rows = mysqli_stmt_affected_rows($stmt);
@@ -37,5 +28,3 @@
 
     /* Devolver JSON */
     echo json_encode($respuestaFinal);
-
-
