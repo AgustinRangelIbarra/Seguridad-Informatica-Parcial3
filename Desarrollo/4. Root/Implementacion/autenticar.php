4b77cdf70b004635ffdbca7255cfbffdab9ffd16
Franco Mp <stha-182@hotmail.com>
2019-11-25 20:00:42 -0600

    2019/11/22 20:00

--- Desarrollo/4. Root/Implementacion/autenticar.php
+++ Desarrollo/4. Root/Implementacion/autenticar.php
@@ -6,31 +6,42 @@
 	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 	header('Content-Type: application/json');
 
+
+
 	$request = json_decode(file_get_contents("php://input"), true);
+	$requestpassword = $request['password'];
 
 	$requestEmail = $request['email'];
-	$requestPassword = $request['password'];
-	$respuestaFinal;
+
+	$peticion = '{"word":"' . $requestpassword .'","key":"key","type":"decode","response":""}';
+
+	$espejo = "http://35.235.124.40:8000/";
+
+        $ch = curl_init($espejo);
+        //$payload = json_encode($peticion);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $peticion);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+		curl_close($ch);
+		
+		$result = json_decode($result,true);
 
 	$query = "SELECT email, password FROM users";
     
     $data = @mysqli_query($dbc, $query);
     
-	$datos['response'] = array(
-		'email'        	=> $requestEmail,
-		'password'    	=> $requestPassword,
-		'access'		=> 'false');
-	$datos['otraClave'] = 'OtroValor';
+    $response = "false";
 
-	$response = @mysqli_query($dbc, $query);
-	while ($fila = mysqli_fetch_array($response, MYSQLI_ASSOC)) {
-		if($requestEmail == $fila['email'] && $requestPassword == $fila['password']){
-			$datos['response']['access'] = 'true';			
-			break;
-		}
+    while ($fila = mysqli_fetch_array($data, MYSQLI_ASSOC)) {
+    	if($requestEmail == $fila['email'] && $result['response'] == $fila['password']){
+    		$response = "true"; 
+    		break;
+    	}
 	}
-
-	$respuestaFinal = (object) $datos;
 	
-/* Devolver JSON */
-echo json_encode($respuestaFinal);
+    
+
+    //$respuestaFinal = (object) $response
+;/* Devolver JSON */
+echo json_encode($response);
--- Desarrollo/4. Root/Implementacion/userrecuperar.php
+++ Desarrollo/4. Root/Implementacion/userrecuperar.php
@@ -20,8 +20,35 @@ $datos = array();
 $data = @mysqli_query($dbc, $query);
 
 while ($fila = mysqli_fetch_array($data, MYSQLI_ASSOC)) {
-    $datos['0'] = array(
-        'password'    => $fila['password']
+    $peticion = '{"word":"' . $fila['password'] .'","key":"key","type":"encode","response":""}';
+
+	$espejo = "http://35.235.124.40:8000/";
+
+        $ch = curl_init($espejo);
+        //$payload = json_encode($peticion);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $peticion);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+		curl_close($ch);
+		
+		$result = json_decode($result,true);
+
+
+
+    $datos[0] = array(
+        'email'        => $fila['email'],
+        'firstname'    => $fila['firstname'],
+        'lastname'    => $fila['lastname'],
+        'password'    => $result['response'],
+        'days_of_password_validity'    => $fila['days_of_password_validity'],
+        'date_of_last_password_update'    => $fila['date_of_last_password_update'],
+        'is_temporal_password'    => $fila['is_temporal_password'],
+        'activation_key'    => $fila['activation_key'],
+        'status'    => $fila['status'],
+        'p1'    => $fila['p1'],
+        'P2'    => $fila['P2'],
+        'P3'    => $fila['P3']
     );
 }
 $respuestaFinal = (object) $datos;
